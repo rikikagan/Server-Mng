fcbc79c0a6b87c2e6efa08ad0af7bc8d0618ab62
Riki <r0533147262@gmail.com>
2024-05-23 19:45:00 +0300

    only server

---
+++ Mng.Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Mng.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mng.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<EmployeeRoles> EmployeeRoles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration["DbConnectionString"]);
+            optionsBuilder.LogTo((message) => Debug.Write(message));
+        }
+       
+
+    }
+}
---
+++ Mng.Services/Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using Mng.Core.Entities;
+using Mng.Core.IRepositories;
+using Mng.Core.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mng.Service.Services
+{
+    public class EmployeeService: IEmployeeService
+    {
+        public readonly IEmployeeRepository _employeeRepository;
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public async Task<Employee> GetByIdAsync(int empRoleId)
+        {
+            return await _employeeRepository.GetByIdAsync(empRoleId);
+        }
+        public async Task<IEnumerable<Employee>> GetListAsync()
+        {
+            return await _employeeRepository.GetListAsync();
+        }
+        public async Task<Employee> AddAsync(Employee employee)
+        {
+            if (!IsValidEmployeeId(employee.TZ))
+                throw new Exception("Invalid employee data.");
+            if (IsDuplicateEmployeeId(employee.TZ))
+                throw new Exception("Employee width the same identity number already exists");
+            return await _employeeRepository.AddAsync(employee);
+        }
+
+        private bool IsValidEmployeeId(string tz)
+        {
+            return !string.IsNullOrEmpty(tz) && tz.Length == 9 && int.TryParse(tz, out _);
+        }
+        private bool IsDuplicateEmployeeId(string tz)
+        {
+            var existingEmployee = _employeeRepository.GetListAsync().Result.FirstOrDefault(e => e.TZ == tz);
+            return existingEmployee != null && existingEmployee.ActivityStatus == true;
+        }
+        public async Task DeleteAsync(int employeeId)
+        {
+            await _employeeRepository.DeleteAsync(employeeId);
+        }
+        public async Task<Employee> UpdateAsync(Employee employee)
+        {
+            return await _employeeRepository.UpdateAsync(employee);
+        }
+    }
+}
